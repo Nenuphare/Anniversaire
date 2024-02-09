32ef686db1caaaae5f15dc347461962770add66a
Nenuphare <caye.damien@gmail.com>
2024-02-09 22:27:01 +0100

    Add Two firsdt components + Some content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { Grid } from '@mui/material';
-import './App.css';
-import Anniv from './components/Anniv.jsx';
-import Header from './components/Header.jsx';
-import Citation from './components/Citation.jsx';
+import { Grid } from "@mui/material";
+import "./App.css";
+import Anniv from "./components/Anniv.jsx";
+import Citation from "./components/Citation.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
-
   return (
+    
     <div className="App">
       <Header />
       <Grid container>
-        <Grid item xs={6} >
-          <Anniv/>
+        {/* La hauteur de chaque Grid est définie à 100% de la hauteur de la vue, en soustrayant la hauteur du Header (64px) */}
+        <Grid
+          item
+          xs={6}
+          style={{ height: "600px", backgroundColor: "#DF80AC" }}
+        >
+          {/* Contenu du composant Citation */}
+          <Anniv />
         </Grid>
 
         <Grid item xs={6}>
+          {/* Contenu du composant Anniv */}
           <Citation />
         </Grid>
       </Grid>
--- src/components/Citation.jsx
+++ src/components/Citation.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from "@mui/material";
 
 const Citation = () => {
-  return (
-    <div>
-      <h1>CITATION</h1>
-    </div>
-  )
-}
+  const border = {
+    borderLeft: '2px solid #000',
+  };
+  const white_back = {
+    backgroundColor: "#FFFFFF",
+    borderRadius: '10px',
+    height: "100px"
+  };
+  const centeredGrid = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    position: 'relative', // Ajout de position relative pour le conteneur parent
+    margin: '10px',
+  };
+  const borderedGridStyle = {
+    border: '2px solid #000',
+    borderRadius: '5px',
+    backgroundColor: '#DF80AC',
+    padding: '5px',
 
-export default Citation
+  };
+
+  return (
+    <div style={border}>
+      <Grid container style={centeredGrid}>
+        <Grid style={borderedGridStyle}>
+          <Grid item style={white_back}>
+            <p>There is a Japan legend that said ...
+              <br />
+              -Japanese Legend
+            </p>
+          </Grid>
+        </Grid>
+        <Grid style={borderedGridStyle}>
+          <Grid item style={white_back}>
+            <p>There is a Japan legend that said ...
+              <br />
+              -Japanese Legend
+            </p>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Citation;
