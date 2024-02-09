e15cafd0d9e78fcd75fc335002387c4fceebf674
Nenuphare <caye.damien@gmail.com>
2024-02-09 22:57:08 +0100

    Changement de DA + Finish

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import { Grid } from "@mui/material";
-import "./App.css";
-import Anniv from "./components/Anniv.jsx";
-import Citation from "./components/Citation.jsx";
-import Header from "./components/Header.jsx";
+import React from 'react';
+import Header from './components/header/header.js';
+import Main from './components/main/main.js';
 
-function App() {
+const App = () => {
   return (
-    
     <div className="App">
-      <Header />
-      <Grid container>
-        {/* La hauteur de chaque Grid est définie à 100% de la hauteur de la vue, en soustrayant la hauteur du Header (64px) */}
-        <Grid
-          item
-          xs={6}
-          style={{ height: "600px", backgroundColor: "#DF80AC" }}
-        >
-          {/* Contenu du composant Citation */}
-          <Anniv />
-        </Grid>
-
-        <Grid item xs={6}>
-          {/* Contenu du composant Anniv */}
-          <Citation />
-        </Grid>
-      </Grid>
+      <Header/>
+      <Main/>
     </div>
   );
 }
